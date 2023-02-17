c436363d42fd8c591099e3d1279c0b57983728f0
joaopedro214 <joaopedrodasilvalima@gmail.com>
2023-02-17 14:29:40 -0300

    FEAT: Standardize columns' values (hard part)

--- src/raw_to_silver.py
+++ src/raw_to_silver.py
@@ -64,7 +64,6 @@ if __name__ == "__main__":
         .load(f"{BASE_PATH}/*.csv")
     )
 
-    df_accidents.show(10)
     # DATA INVERSA - DATE
     # formats: yyyy-MM-dd, dd/MM/yyyy and dd/MM/yy
 
@@ -166,15 +165,133 @@ if __name__ == "__main__":
 
     # causa_acidente - STRING
     # ...
+    df_accidents = lowercase_column(df_accidents, "causa_acidente")
+    df_accidents = replace_nulls(df_accidents, "causa_acidente", F.lit(None))
+    replace_by = [
+        (F.lit(None), "outras"),
+        ("Falta de atenção", ["Falta de atenção","Falta de Atenção à Condução"]),
+        ("Ingestão de álcool ou psicoativos", [
+                "Ingestão de substâncias psicoativas pelo condutor", 
+                "Ingestão de álcool pelo condutor", "Ingestão de álcool", 
+                "Ingestão de Substâncias Psicoativas", "Ingestão de Álcool"
+        ]),
+        ("Não guardar distância de segurança",[
+            "Não guardar distância de segurança", 
+            "Condutor deixou de manter distância do veículo da frente"
+        ]),
+        ("Defeito Mecânico no Veículo",[
+            "Problema com o freio", "Problema na suspensão", 
+            "Defeito mecânico em veículo", 
+            "Avarias e/ou desgaste excessivo no pneu", 
+            "Defeito Mecânico no Veículo", 
+            "Demais falhas mecânicas ou elétricas", "Faróis desregulados",
+            "Deficiência ou não Acionamento do Sistema de Iluminação/Sinalização do Veículo"
+        ]),
+        ("Defeito na via",[
+            "Demais falhas na via", 
+            "Defeito na via", "Defeito na Via", 
+            "Pista esburacada", 
+            "Falta de acostamento",
+            "Afundamento ou ondulação no pavimento",
+            "Falta de elemento de contenção que evite a saída do leito carroçável"
+        ]),
+        ("Condutor dormindo",[
+            "Dormindo", "Condutor Dormindo"
+        ]),
+        ("Desobediência à sinalização",[
+            "Desobediência à sinalização", 
+            "Condutor desrespeitou a iluminação vermelha do semáforo"
+        ]),
+        ("Desobediência às normas de trânsito pelo condutor",[
+            "Desobediência às normas de trânsito pelo condutor", 
+            "Desrespeitar a preferência no cruzamento",
+        ]),
+        ("Mal Súbito",[
+            "Mal Súbito", "Mal súbito do condutor"
+        ]),
+        ("Reação tardia ou ineficiente do condutor",[
+            "Reação tardia ou ineficiente do condutor", 
+            "Ausência de reação do condutor"
+        ]),
+        ("Fenômenos da Natureza",[
+            "Fenômenos da Natureza",
+            "Demais Fenômenos da natureza",
+            "Chuva", "Neblina", "Fumaça"
+        ]),
+        ("Desobediência às normas de trânsito pelo pedestre",[
+            "Pedestre andava na pista", 
+            "Entrada inopinada do pedestre", 
+            "Desobediência às normas de trânsito pelo pedestre", 
+            "Pedestre cruzava a pista fora da faixa"
+        ]),
+        ("Ingestão de álcool ou psicoativos pelo pedestre",[
+            "Ingestão de álcool ou de substâncias psicoativas pelo pedestre", 
+            "Ingestão de álcool e/ou substâncias psicoativas pelo pedestre"
+        ]),
+        ("Transitar no acostamento",[
+            "Transitar no acostamento", 
+            "Transitar na calçada"
+        ]),
+        ("Acumulo de material sobre o pavimento",[
+            "Acumulo de areia ou detritos sobre o pavimento", 
+            "Acumulo de óleo sobre o pavimento", 
+            "Acumulo de água sobre o pavimento"
+        ])
+    ]
+
+    replace_causa_acidente_when = F.when(
+        F.col("causa_acidente").isin( replace_by[0][1] ), replace_by[0][0]
+    )
+    for replace_value, values_to_replace in replace_by[1:]:
+        values_to_replace = [ value.lower() for value in values_to_replace ]
+        replace_causa_acidente_when = replace_causa_acidente_when.when(
+            F.col("causa_acidente").isin( values_to_replace ),
+            replace_value
+        )
+    
+    replace_causa_acidente_when = replace_causa_acidente_when.otherwise(F.col("causa_acidente"))
+
+    df_accidents = (
+        df_accidents
+        .withColumn(
+            "causa_acidente",
+            replace_causa_acidente_when
+        )
+    )
 
     # tipo_acidente - STRING
     # preproces: lowercase
     # if contains "colisão" and "objeto" -> "colisão com objeto"
     # if contains "queda" -> "queda do veiculo"
     # replace: "Pedestre" = "pessoa"
-    # normalize: eventos atípicos, danos eventuais
     # normalize: saída de pista, saída de leito carroçável
     # replace: null (str) for null
+    df_accidents = lowercase_column(df_accidents, "tipo_acidente")
+    df_accidents = replace_nulls(df_accidents, "tipo_acidente", F.lit(None))
+    df_accidents = (
+        df_accidents
+        .withColumn(
+            "tipo_acidente",
+            F
+            .when(
+                F.col("tipo_acidente").contains("colisão")
+                & F.col("tipo_acidente").contains("objeto"),
+                "colisão com objeto"
+            )
+            .when(
+                F.col("tipo_acidente").contains("queda"),
+                "queda do veiculo"
+            )
+            .when(
+                F.col("tipo_acidente").contains("pedestre"),
+                F.regexp_replace( F.col("tipo_acidente"), "pedestre", "pessoa" )
+            )
+            .when(
+                F.col("tipo_acidente") == "saída de leito carroçável",
+                "saída de pista"
+            )
+        )
+    )
 
     # classificacao_acidente - STRING
     # replace: null and (null) for Ignorado
